3ebbcee293aceec488bdd49b95e79879f6dccf53
Wolfinhoc <broccowolfardt@gmail.com>
2025-08-19 21:43:54 -0300

    Update desafio_Mod1_DM_CC4.dart

--- desafio_Mod1_DM_CC4.dart
+++ desafio_Mod1_DM_CC4.dart
@@ -1,14 +1,10 @@
 void main() {
-  // Crie um programa que declare variáveis para armazenar o nome de um produto, a quantidade em estoque, o preço unitário e se está disponível para venda. Atribua valores e exiba-os no console.
-  String nomeProduto = "Fone de Ouvido Bluetooth";
-  int quantProduto = 75;
-  double precoProduto = 129.99;
-  bool estaDisponivelParaVenda = true;
+  String firstName = 'João';
+  String lastName = 'Silva';
 
- 
-  print("--- Detalhes do Produto ---");
-  print("Nome: $nomeProduto");
-  print("Quantidade em Estoque: $quantProduto unidades");
-  print("Preço Unitário: R\$${precoProduto.toStringAsFixed(2)}"); 
-  print("Disponível para Venda: ${estaDisponivelParaVenda ? 'Sim' : 'Não'}");
+  String fullNameConcatenation = firstName + ' ' + lastName;
+  print('Nome completo (concatenação): ' + fullNameConcatenation);
+
+  String fullNameInterpolation = '$firstName $lastName';
+  print('Nome completo (interpolação): $fullNameInterpolation');
 }
