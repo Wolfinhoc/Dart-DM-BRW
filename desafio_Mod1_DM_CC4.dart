d7c1191f94c2256c2976e8bc481025093e1982d9
Wolfinhoc <broccowolfardt@gmail.com>
2025-08-19 21:50:11 -0300

    Update desafio_Mod1_DM_CC4.dart

--- desafio_Mod1_DM_CC4.dart
+++ desafio_Mod1_DM_CC4.dart
@@ -1,10 +1,24 @@
 void main() {
-  String firstName = 'João';
-  String lastName = 'Silva';
 
-  String fullNameConcatenation = firstName + ' ' + lastName;
-  print('Nome completo (concatenação): ' + fullNameConcatenation);
+  double a = 0.1;
+  double b = 0.2;
+  double c = 0.3;
 
-  String fullNameInterpolation = '$firstName $lastName';
-  print('Nome completo (interpolação): $fullNameInterpolation');
+  double sum = a + b;
+
+  print('Demonstrating floating-point precision error:');
+  print('------------------------------------------------');
+  print('Value a: $a');
+  print('Value b: $b');
+  print('Expected sum c: $c');
+  print('Calculated sum (a + b): $sum');
+
+  bool areEqual = (sum == c);
+
+  print('------------------------------------------------');
+  print('Is (a + b) equal to c? (i.e., $sum == $c)?');
+  print('Result: $areEqual');
+  print('\nExplanation: Due to the binary representation of floating-point numbers,');
+  print('0.1, 0.2, and 0.3 cannot be precisely represented. This leads to small');
+  print('discrepancies that can make direct equality comparisons unreliable.');
 }
